ef5c84e63c3636d81207fcc9031b859838c60675
Thomas <contact@thomas-george.com>
2021-05-24 20:33:28 +0200

    Initial Commit

---
+++ Project_6_Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_6_Animations
+//
+//  Created by Thomas George on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_6_Animations/Project_6_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6_AnimationsApp.swift
+//  Project_6_Animations
+//
+//  Created by Thomas George on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_6_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
